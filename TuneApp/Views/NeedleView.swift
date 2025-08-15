95478ff89c921088f0c282c8c4b0a09649bd495f
zauTT <giozau1@gmail.com>
2025-08-15 16:45:59 +0400

    added arc and ticks to the needle. is starts from left to right going through the top. making it visually more acceptable

--- TuneApp/Views/NeedleView.swift
+++ TuneApp/Views/NeedleView.swift
@@ -11,20 +11,17 @@ import UIKit
 final class NeedleView: UIView {
     private let needle = UIView()
     private let dialLayer = CAShapeLayer()
+    private var tickLayers: [CAShapeLayer] = []
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .clear
-        setupDial()
+        layer.addSublayer(dialLayer)
         setupNeedle()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupDial() {
-        layer.addSublayer(dialLayer)
     }
 
     private func setupNeedle() {
@@ -33,31 +30,68 @@ final class NeedleView: UIView {
         needle.layer.anchorPoint = CGPoint(x: 0.5, y: 1.0)
         addSubview(needle)
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         needle.center = CGPoint(x: bounds.midX, y: bounds.midY)
-        updateDialPath()
-    }
-
-    private func updateDialPath() {
-        let radius = min(bounds.width, bounds.height) / 2 - 20
-        let center = CGPoint(x: bounds.midX, y: bounds.midY)
-        let startAngle = CGFloat.pi * 5 / 4
-        let endAngle = CGFloat.pi * -1 / 4
-
-        let path = UIBezierPath(arcCenter: center,
-                                radius: radius,
-                                startAngle: startAngle,
-                                endAngle: endAngle,
-                                clockwise: true)
         
-        dialLayer.path = path.cgPath
-        dialLayer.strokeColor = UIColor.systemGray4.cgColor
-        dialLayer.lineWidth = 8
-        dialLayer.fillColor = UIColor.clear.cgColor
+        if dialLayer.path == nil {
+            updateDialPath()
+        }
     }
-
+    
+    private func updateDialPath() {
+        let needleLength = needle.frame.height
+        let radius = needleLength
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        let startAngle = CGFloat.pi
+        let endAngle = CGFloat(0)
+        
+        let arcPath = UIBezierPath(arcCenter: center,
+                                   radius: radius,
+                                   startAngle: startAngle,
+                                   endAngle: endAngle,
+                                   clockwise: true)
+        dialLayer.path = arcPath.cgPath
+        dialLayer.strokeColor = UIColor.systemGray4.cgColor
+        dialLayer.lineWidth = 4
+        dialLayer.fillColor = UIColor.clear.cgColor
+        
+        tickLayers.forEach { $0.removeFromSuperlayer() }
+        tickLayers.removeAll()
+        
+        let tickCount = 11
+        let tickLength: CGFloat = 10
+        let arcRadius = radius
+        
+        for i in 0..<tickCount {
+            let tickAngle = startAngle + (endAngle + startAngle) * CGFloat(i) / CGFloat(tickCount - 1)
+            
+            let outerPoint = CGPoint(
+                x: center.x + arcRadius * cos(tickAngle),
+                y: center.y + arcRadius * sin(tickAngle)
+            )
+            
+            let innerPoint = CGPoint(
+                x: center.x + (arcRadius - tickLength) * cos(tickAngle),
+                y: center.y + (arcRadius - tickLength) * sin(tickAngle)
+            )
+            
+            let tickPath = UIBezierPath()
+            tickPath.move(to: innerPoint)
+            tickPath.addLine(to: outerPoint)
+            
+            let tickLayer = CAShapeLayer()
+            tickLayer.path = tickPath.cgPath
+            tickLayer.strokeColor = UIColor.label.cgColor
+            tickLayer.lineWidth = 2
+            
+            layer.addSublayer(tickLayer)
+            tickLayers.append(tickLayer)
+        }
+    }
+    
     func update(cents: Double) {
         let clamped = max(-50, min(50, cents))
         let angle = CGFloat(clamped / 50) * (.pi / 4)
