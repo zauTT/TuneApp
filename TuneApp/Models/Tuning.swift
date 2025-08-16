f40388d75dfabb0d79850176a94a37702fd1cfd8
zauTT <giozau1@gmail.com>
2025-08-16 15:19:50 +0400

    updated Auto tune displaying which string is being tuned

--- TuneApp/Models/Tuning.swift
+++ TuneApp/Models/Tuning.swift
@@ -13,11 +13,11 @@ struct Tuning {
     //MARK: - Standard Tuning - E A D G B E
     
     static let standardGuitarFrequencies: [String: Double] = [
-        "E2": 82.4069,
-        "A2": 110.0,
-        "D3": 146.8324,
-        "G3": 195.9977,
-        "B3": 246.9417,
-        "E4": 329.6276
+        "Low E": 82.4069,
+        "A": 110.0,
+        "D": 146.8324,
+        "G": 195.9977,
+        "B": 246.9417,
+        "high E": 329.6276
     ]
 }
